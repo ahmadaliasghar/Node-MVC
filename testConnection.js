8d5af07e01531352ac8ea83b8eab4a3e70ecea54
Ahmad Ali <ahmadali2882@yahoo.com>
2024-08-08 12:46:13 +0500

    Added .env.example

--- testConnection.js
+++ testConnection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://ahmadaliasghar:admin123@cluster0.utrz0qp.mongodb.net/calendertask?retryWrites=true&w=majority', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       
       useUnifiedTopology: true,
